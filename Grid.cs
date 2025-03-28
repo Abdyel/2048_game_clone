8c1c6d4afe361213f15f4b521c691c1b98f03b59
Abdyel <noah@abdyel.com>
2025-03-27 20:11:07 -0500

    edited grid

--- Grid.cs
+++ Grid.cs
@@ -21,7 +21,7 @@ public partial class Grid : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		
 	}
 	public override void _Input(InputEvent @event)
 	{
