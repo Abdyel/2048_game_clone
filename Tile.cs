af8d2339875935a7b31b16df12945a59204d220a
Abdyel <noah@abdyel.com>
2025-04-01 19:27:11 -0500

    Added TODO for color implementation for tiles

--- Tile.cs
+++ Tile.cs
@@ -5,6 +5,8 @@ public partial class Tile : Polygon2D
 {
 	private int value = 0;
 	private Label label;
+	private Color tilecolor;
+	private int currentDigits;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -42,9 +44,37 @@ public partial class Tile : Polygon2D
 
 		positionAnim.TweenProperty(this,"position", Position - new Vector2(50,50), 0.2f);
 	}
+
 	private void UpdateLabel()
 	{
 		label = GetNode<Label>("Label");
 		label.Text = value.ToString();
+
+		/*	// CODE USE TO MANUALLY CENTER LABEL INSIDE TILE. EDITOR USED TO AUTO CONTROL CENTERING LABEL.
+		switch(value.ToString().Length)
+		{
+			case 2:
+				if(currentDigits < 2)
+				{
+					label.Position = new Vector2(label.Position.X, label.Position.Y);
+					currentDigits = 2;
+				}
+				break;
+			case 3:
+				if(currentDigits < 3)
+				{
+					label.Position = new Vector2(label.Position.X, label.Position.Y);
+					currentDigits = 3;
+				}
+				break;
+			case 4:
+				if(currentDigits < 4)
+				{
+					label.Position = new Vector2(label.Position.X, label.Position.Y);
+					currentDigits = 4;
+				}
+				break;
+		}
+		*/
 	}
 }
